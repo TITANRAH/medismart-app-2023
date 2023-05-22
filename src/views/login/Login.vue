d1493ee270de7f08c7a3e0a7ac1121a6cc4ade6d
sergio <granrah1@gmail.com>
2023-05-22 01:59:30 -0400

    first commit

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,24 +1,45 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import { useClienteStore } from '../../stores/cliente-store/cliente-store';
+// import { computed, ref } from 'vue';
 // import fondoLogin from  `../../assets/${clienteStore.currentCss.replace('.css','')}/img-login.svg`
-import fondoLogin from  `../../assets/copeuch/img-login.svg`
-
-
 const clienteStore = useClienteStore()
+const fondo = ref('')
+
+
+
+onMounted(() => {
+
+    if (clienteStore.currentCss === 'copeuch.css') {
+
+        console.log(clienteStore.currentCss)
+
+        fondo.value = '../assets/copeuch/img-login.svg'
+    } else {
+
+        fondo.value = '../../assets/medismart/img-login.svg'
+    }
+})
+
+
+
+
+
+
 
 
 </script>
 <template>
     <div class="container">
-    <div class="columna-izquierda">
+        <div class="columna-izquierda">
 
-        {{ clienteStore.currentCss.replace('.css','') }}
+           
+{{ fondo }}
+            <img :src="fondo" alt="">
 
-        <img src={{fondoLogin}} alt="">
-       
+        </div>
+        <div class="columna-derecha">Contenido de la columna derecha</div>
     </div>
-    <div class="columna-derecha">Contenido de la columna derecha</div>
-</div>
 </template>
 
 
