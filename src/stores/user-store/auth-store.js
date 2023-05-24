d9d2cebdaef4b758040fcefc977da56bb39643bc
sergio <granrah1@gmail.com>
2023-05-23 22:14:47 -0400

    local storage

--- src/stores/user-store/auth-store.js
+++ src/stores/user-store/auth-store.js
@@ -2,46 +2,45 @@ import axios from "axios";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useAuthStore = defineStore(
-  "auth",
-  () => {
-    const url = "https://api-auth-ms-prod.azurewebsites.net/api/Login";
+export const useAuthStore = defineStore("auth", () => {
+  const url = "https://api-auth-ms-prod.azurewebsites.net/api/Login";
 
-    const userDataRes = ref({});
-    const userToken = ref('');
-    const userServices = ref([])
+  const user = ref(null);
 
-    const getUser = async (userName, password) => {
-      const dataUser = {
-        userName: userName,
-        password: password,
-      };
-
-      //   const headers = {"Accept": "application/json", "Content-Type": "application/json"};
-      try {
-        const {data} = await axios.post(url, dataUser);
-
-        userDataRes.value = data;
-        userToken.value = data.token;
-        userServices.value = data.userData.homeServices;
-
-
-
-
-
-        console.log("hizo login userData: ", userDataRes.value);
-        console.log("hizo login userToken: ", userToken.value);
-        console.log("hizo login userServices: ", userServices.value);
-      } catch (e) {
-        console.log("cayo en el error", e);
-      } finally {
-        console.log("cayo en el finalluy");
-      }
+  const getUser = async (userName, password) => {
+    const dataUser = {
+      userName: userName,
+      password: password,
     };
 
-    return {
-      userDataRes,
-      getUser,
-      userServices
-    };
-  });
+    //   const headers = {"Accept": "application/json", "Content-Type": "application/json"};
+    try {
+      const { data } = await axios.post(url, dataUser);
+
+      user.value = data;
+
+      localStorage.setItem("user", JSON.stringify(user.value));
+
+      console.log("user: ", user.value);
+    } catch (e) {
+      console.log("cayo en el error", e);
+    } finally {
+      console.log("cayo en el finalluy");
+    }
+  };
+
+  const getUserLocal = () => {
+    if (localStorage.getItem("user")) {
+      // tomar datos del local
+      user.value = JSON.parse(localStorage.getItem("user"));
+
+      console.log("user desde validacion de get item",user.value );
+    }
+  };
+
+  return {
+    user,
+    getUser,
+    getUserLocal,
+  };
+});
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,29 +1,32 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+
 import { useAuthStore } from '../../stores/user-store/auth-store';
 import { storeToRefs } from 'pinia';
 
 
-const authStore = useAuthStore()
-const {userServices, userDataRes} = storeToRefs(authStore)
+const useAuth = useAuthStore()
 
+const { user } = storeToRefs(useAuth)
+
+
+useAuth.getUserLocal()
 
 
 
 </script>
 
 <template>
-    <p v-if="userServices.length === 0">Sin Servicios</p>
+    <p v-if="user === null">Sin Usuario</p>
     <div v-else>
 
-        <h1>Hola {{ userDataRes.userData.name }}</h1>
+        <h1>Hola {{ user.userData.name }}</h1>
 
-        <ul>
-            <li v-for="(service, index) in userServices" :key="index">
-                {{ service.cod }}
-            </li>
-        </ul>
-       
+        <!-- <ul>
+                <li v-for="(service, index) in userServices" :key="index">
+                    {{ service.cod }}
+                </li>
+            </ul> -->
+
     </div>
 </template>
 
